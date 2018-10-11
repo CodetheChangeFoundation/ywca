bc0402a0d437a8136019264b2ffdd9730c06a579
Sumeet Grewal <32283422+sumeetgrewal@users.noreply.github.com>
2018-10-10 19:39:33 -0700

    Update functions.php

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@
 function bootstrapstarter_enqueue_styles() {
     wp_register_style('bootstrap', get_template_directory_uri() . '/bootstrap/css/bootstrap.min.css' );
     $dependencies = array('bootstrap');
-    wp_enqueue_style( 'bootstrapstarter-style', get_stylesheet_directory_uri(), $dependencies ); 
+    wp_enqueue_style( 'bootstrapstarter-style', get_stylesheet_uri(), $dependencies ); 
 }
  
 function bootstrapstarter_enqueue_scripts() {
