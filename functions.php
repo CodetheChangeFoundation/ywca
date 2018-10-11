60e0a64670314058af761415cd4bd0124628c57b
Nandini Bulusu <nanbul785@gmail.com>
2018-10-10 19:47:56 -0700

    Hardcoded HTML footer

--- functions.php
+++ functions.php
@@ -1,17 +1,17 @@
 <?php
- 
+
 function bootstrapstarter_enqueue_styles() {
     wp_register_style('bootstrap', get_template_directory_uri() . '/bootstrap/css/bootstrap.min.css' );
     $dependencies = array('bootstrap');
-    wp_enqueue_style( 'bootstrapstarter-style', get_stylesheet_uri(), $dependencies ); 
+    wp_enqueue_style( 'bootstrapstarter-style', get_stylesheet_uri(), $dependencies );
 }
- 
+
 function bootstrapstarter_enqueue_scripts() {
     $dependencies = array('jquery');
     wp_enqueue_script('bootstrap', get_template_directory_uri().'/bootstrap/js/bootstrap.min.js', $dependencies, '3.3.6', true );
 }
- 
+
 add_action( 'wp_enqueue_scripts', 'bootstrapstarter_enqueue_styles' );
 add_action( 'wp_enqueue_scripts', 'bootstrapstarter_enqueue_scripts' );
- 
+
 ?>
