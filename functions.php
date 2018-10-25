fdd9fde9708d0e6b11c8c5c551cb07ca4d2353b9
ariellem99 <ariellem99@gmail.com>
2018-10-24 18:13:53 -0700

    updated master branch

--- functions.php
+++ functions.php
@@ -1,7 +1,12 @@
 <?php
 
-function bootstrap_enqueue_styles () {
-    wp_enqueue_style('bootstrap', get_template_directory_uri() . '/bootstrap/css/bootstrap.min.css');
+function bootstrapstarter_enqueue_styles() {
+    wp_register_style('bootstrap', get_template_directory_uri() . '/bootstrap/css/bootstrap.min.css' );
+    $dependencies = array('bootstrap');
+    wp_enqueue_style( 'bootstrapstarter-style', get_stylesheet_uri(), $dependencies );
+}
+
+function bootstrapstarter_enqueue_scripts() {
     $dependencies = array('jquery');
     wp_enqueue_script('bootstrap', get_template_directory_uri() . '/bootstrap/js/bootstrap.min.js', $dependencies, '3.3.6', true );
 
@@ -11,7 +16,34 @@ function ywca_enqueue_styles() {
     wp_enqueue_style('customcss', get_template_directory_uri() . '/assets/css/ywca.css', array(), '1.0', 'all');
     wp_enqueue_script('customjs', get_template_directory_uri() . '/assets/js/ywca.js', array(), '1.0.0', true);
 }
-add_action( 'wp_enqueue_scripts', 'bootstrap_enqueue_styles' );
-add_action( 'wp_enqueue_scripts', 'ywca_enqueue_styles' );
 
-?>]
+add_action( 'wp_enqueue_scripts', 'bootstrapstarter_enqueue_styles' );
+add_action( 'wp_enqueue_scripts', 'bootstrapstarter_enqueue_scripts' );
+
+function wpb_custom_new_menu() {
+  register_nav_menu('my-custom-menu',__( 'My Custom Menu' ));
+}
+add_action( 'init', 'wpb_custom_new_menu' );
+
+/* Theme setup */
+add_action( 'after_setup_theme', 'wpt_setup' );
+    if ( ! function_exists( 'wpt_setup' ) ):
+        function wpt_setup() {
+            register_nav_menu( 'primary', __( 'Primary navigation', 'wptuts' ) );
+        } endif;
+
+function wpt_register_js() {
+    wp_register_script('jquery.bootstrap.min', get_template_directory_uri() . '/js/bootstrap.min.js', 'jquery');
+    wp_enqueue_script('jquery.bootstrap.min');
+}
+add_action( 'init', 'wpt_register_js' );
+function wpt_register_css() {
+    wp_register_style( 'bootstrap.min', get_template_directory_uri() . '/css/bootstrap.min.css' );
+    wp_enqueue_style( 'bootstrap.min' );
+}
+add_action( 'wp_enqueue_scripts', 'wpt_register_css' );
+
+?>
+<?php // Register custom navigation walker
+    require_once('wp_bootstrap_navwalker.php');
+?>
--- index.php
+++ index.php
@@ -1,20 +1,38 @@
 <?php get_header(); ?>
 
-<h1><?php bloginfo('title'); ?></h1>
- 
+<h1>
+  <?php bloginfo('title'); ?>
+  <?php
+    if(have_posts()):
+      while(have_posts()): the_post(); ?>
+      <h1><?php echo the_title(); ?></h1>
+      <?php endwhile;
+    endif;
+  ?>
+  <?php
+    if(!get_field('enable_title_section')):
+      if(get_field('title_section')): ?>
+        <p><?php echo get_field('title_section') ?></p>
+      <?php endif;
+    endif;
+  ?>
+
+</h1>
+<?php the_content(); ?>
+
 <h3><?php bloginfo('description'); ?></h3>
- 
 <?php while (have_posts()) { the_post(); ?>
- 
- 
-<h2 class="strong">
+
+<h5><?php the_date(); ?></h5>
+
+
+<h2>
 <a href="index.php?p=<?php the_ID(); ?>">
-<?php the_title(); ?>
+
 </a>
 </h2>
- 
-<?php the_content(); ?>
- 
-<?php } ?>
 
-<?php get_footer();?>
+<hr/>
+<?php } ?>
+</body>
+</html>
