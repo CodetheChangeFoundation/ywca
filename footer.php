e9e682c4b9760f98e9ee849dde657092e434c07f
Nandini Bulusu <nanbul785@gmail.com>
2018-10-28 18:11:26 -0700

    Added link to website and logo

--- footer.php
+++ footer.php
@@ -1,12 +1,18 @@
 
 <footer class="footer">
-  <p><div class = "vl"><strong>YWCA Metro Vancouver</strong> is a registered charity, providing a range of integrated services for women and their families, and those seeking to improve the quality of their lives. From early learning and care to housing, health and fitness, employment services and leadership. YWCA Metro Vancouver touches lives in communities throughout the Lower Mainland.</div></p>
-  <p>
-    &copy <?php echo date("Y"); ?> YWCA. Developed by Code the Change Foundation.
-  </p>
-  <p>
-  <a href="#">Back to top</a>
-</p>
+  <div class="footer-container">
+    <img src= "/assets/images/logo.png" width="24" height="39">
+    <div>
+      <p>
+        <div class = "vl">
+          <strong>YWCA Metro Vancouver</strong> is a registered charity, providing a range of integrated services for women and their families, and those seeking to improve the quality of their lives. From early learning and care to housing, health and fitness, employment services and leadership. YWCA Metro Vancouver touches lives in communities throughout the Lower Mainland.
+        </div>
+      </p>
+      <p>
+        &copy <?php echo date("Y"); ?> YWCA. <a href="http://codethechange.ca"> Developed by Code the Change Foundation.</a>
+      </p>
+    </div>
+  </div>
 </footer>
 <?php wp_footer() ?>
 
