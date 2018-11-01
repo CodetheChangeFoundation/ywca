2b24df23f4466a9ca9bc688bfae5069900b6f412
Nandini Bulusu <nanbul785@gmail.com>
2018-10-31 19:13:47 -0700

    Finished hardcoded footer

--- footer.php
+++ footer.php
@@ -1,21 +1,19 @@
 
-<footer class="footer">
-  <div class="footer-container">
-    <img src= "/assets/images/logo.png" width="24" height="39">
-    <div>
-      <p>
-        <div class = "vl">
-          <strong>YWCA Metro Vancouver</strong> is a registered charity, providing a range of integrated services for women and their families, and those seeking to improve the quality of their lives. From early learning and care to housing, health and fitness, employment services and leadership. YWCA Metro Vancouver touches lives in communities throughout the Lower Mainland.
+  <footer class="footer">
+    <div class="flex-container">
+          <div>
+            <img id="vl" src="http://localhost:8888/wp-content/uploads/2018/10/YWCA_MetroVancouver_HorizontalGreen.png" width="140">
+          </div>
+          <div>
+              <p>
+                <strong>YWCA Metro Vancouver</strong> is a registered charity, providing a range of integrated services for women and their families, and those seeking to improve the quality of their lives. From early learning and care to housing, health and fitness, employment services and leadership. YWCA Metro Vancouver touches lives in communities throughout the Lower Mainland.
+              </p>
+          <div>
         </div>
-      </p>
-      <p>
-        &copy <?php echo date("Y"); ?> YWCA. <a href="http://codethechange.ca"> Developed by Code the Change Foundation.</a>
-      </p>
+        <p>
+          &copy <?php echo date("Y"); ?> YWCA. <a href="http://codethechange.ca"> Developed by Code the Change Foundation.</a>
+        </p>
     </div>
   </div>
-</footer>
-<?php wp_footer() ?>
-
-</body>
-
-</html>
+  </footer>
+  <?php wp_footer() ?>
