ff3d979601704b60aa18bd828081e440e916746f
ArielleMurad <ariellem99@gmail.com>
2018-11-07 18:07:10 -0800

    ywca-4: created page templates for about and registration pages

---
+++ page-registration.php
@@ -0,0 +1,32 @@
+<?php
+/*
+  Template Name: Registration Page
+*/
+get_header(); ?>
+
+<h1><?php get_template_part('title'); ?></h1>
+
+<h3><?php bloginfo('description'); ?></h3>
+
+<?php
+if (have_posts()) {
+    while (have_posts()) {
+        the_post();
+        ?>
+        <article class="post">
+        <p><?php the_content(); ?></p>
+    </article>
+    <?php
+} // end while
+} // end if
+?>
+
+<!-- <h5><?php the_date(); ?></h5> -->
+
+<h2>
+  <a href="index.php?p=<?php the_ID(); ?>"></a>
+</h2>
+
+<?php  ?>
+</body>
+</html>
