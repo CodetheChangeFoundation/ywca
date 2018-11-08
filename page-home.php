51e42a4c3f54fbc52957abf1968e1950457d1c51
Kevin Lapeyre <doclap77@gmail.com>
2018-11-07 18:43:17 -0800

    Started home page template

---
+++ page-home.php
@@ -0,0 +1,23 @@
+<?php get_header(); ?>
+
+
+<div class="img-container"> 
+    <img class="home-page-img" src="<?php echo get_template_directory_uri() . '/assets/images/home-page-image.jpg'; ?>"/>
+</div>
+
+<div>Welcome!</div>
+
+<div class="mid-section">
+    Mid section
+</div>
+
+<div>Bottom List with Button</div>
+
+
+
+
+
+ 
+
+
+<?php get_footer();?>
